2a7c24447587afd02cd5ee38046c27b04b091c57
raashed <mohammedraashed10@gmail.com>
2024-11-24 19:52:05 +0530

    understanding databases

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,6 +2,28 @@ const express = require("express");
 const connectDB = require('./config/database');
 const app = express();
 const port = 5555;
+const User = require("./models/user")
+
+app.post("/signup", async (req, res) => {
+    const userObj = {
+        firstName : "Mohammed",
+        lastName : "Raashed",
+        emailId : "raashed@gmail.com",
+        password : "12345"
+    }
+
+    //creating the new instance of the user model
+    const user = new User(userObj);
+
+    try {
+        await user.save();
+        res.send("user added successfully..");
+    }
+    catch(err){
+        res.status(400).send("Error saving the user");
+    }
+
+})
 
 // still learning and didnt pushed my (database-api)'s file on github
 // right now added it to .gitignore in future will add it to .env file
@@ -12,8 +34,8 @@ connectDB()
     app.listen(port, () => {
         console.log(`server is successfully listening on port ${port}`); 
     });
-})
-.catch((err) => {
-    console.error("Database cannot be connected");
+    })
+    .catch((err) => {
+        console.error("Database cannot be connected");
 }) 
 
