8b5ba7a6e121a3f3419fc692797b84f6e0492421
raashed <mohammedraashed10@gmail.com>
2024-11-24 14:46:19 +0530

    correct way to connect to the database

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,19 +1,16 @@
 const express = require("express");
-const { adminAuth, userAuth } = require("./middlewares/Auth");
+const connectDB = require('./config/database');
 const app = express();
 const port = 5555;
 
-app.use("/admin", adminAuth);
-app.use("/user", userAuth);
-
-app.get("/admin/getAllData", (req, res) => {
-    res.send("All data send")
+connectDB()
+    .then(() => {
+    console.log("Database connection was established..");
+    app.listen(port, () => {
+        console.log(`server is successfully listening on port ${port}`); 
+    });
 })
+.catch((err) => {
+    console.error("Database cannot be connected");
+}) 
 
-app.get("/user/home", userAuth, (req, res) => {
-    res.send("data is ready to load")
-})
-
-app.listen(port, () => {
-    console.log(`server is successfully listening on port ${port}`); 
-});
