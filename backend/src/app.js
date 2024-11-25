2c23019f1ee04a832c9d954f1d4ed343ea245dcd
raashed <mohammedraashed10@gmail.com>
2024-11-25 06:53:01 +0530

    simple CRUD operations with mongoose

--- backend/src/app.js
+++ backend/src/app.js
@@ -6,6 +6,7 @@ const User = require("./models/user")
 
 app.use(express.json());
 
+// adding users to the database
 app.post("/signup", async (req, res) => {
 
     //creating the new instance of the user model
@@ -15,11 +16,59 @@ app.post("/signup", async (req, res) => {
         res.send("user added successfully..");
     }
     catch(err){
-        res.status(400).send("Error occured during saving the user");
+        res.status(404).send("Error occured during saving the user");
     }
 
 })
 
+// get users by email
+app.get("/user", async (req, res) => {
+    const userEmail = req.body.emailId;
+    try {
+        const user = await User.findOne({emailId : userEmail});
+        if (!user){
+            res.status(404).send("User not found");
+        } else {
+            res.send(user);
+        }
+    } catch (err) {
+        res.status(404).send("Something went wrong..")
+    }
+})
+
+// FEED-API | getting all the users from the database
+app.get("/feed", async(req, res) => {
+    try {
+        const users = await User.find();
+        res.send(users);
+    } catch (err) {
+        res.status(404).send("Something went wrong..")
+    }
+})
+
+// deleting user from the database
+app.delete("/user", async(req, res) => {
+    const userId = req.body.userId;
+    try {
+        const user = await User.findByIdAndDelete(userId)
+        res.send("User deleted successfully");
+    } catch (err) {
+        res.status(404).send("Something went wrong")
+    }
+})
+
+// update data of the user
+app.patch("/user", async(req, res) => {
+    const userId =  req.body.userId;
+    const data = req.body;
+
+    try {
+        const user = await User.findByIdAndUpdate(userId, data);
+        res.send("User updated successfully");
+    } catch (err) {
+        res.status(404).send("couldnt update the information")
+    }
+})
 // still learning and didnt pushed my (database-api)'s file on github
 // right now added it to .gitignore in future will add it to .env file
 
